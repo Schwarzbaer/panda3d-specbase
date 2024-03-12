91e2025c6f142511c3d82e5c2d0ad899dc5490ad
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2024-03-12 19:38:06 +0100

    Refactored specs.

--- bin/demo.py
+++ bin/demo.py
@@ -1,18 +1,17 @@
 from panda3d.core import Vec4
 
 from specbase import SpecBase
+from specbase import SBWindow, SBSceneGraph, SBCamera, SBDisplayRegion
 from specbase import load_model, refov
-from specbase import RendererElement
-from specbase import SBWindow, SBSceneGraph, SBCamera, SBDisplayRegion, SBKeep
 
 
-spec = {
-    RendererElement.WINDOW: [SBWindow('win')],
-    RendererElement.SCENE_GRAPH: [SBSceneGraph('render')],
-    RendererElement.CAMERA: [SBCamera('cam')],
-    RendererElement.DISPLAY_REGION: [SBDisplayRegion('win_dr3d', 'win', 'cam', dimensions=Vec4(0.0, 0.5, 0.0, 1.0))],
-}
-SpecBase(spec)
+first_spec = [
+    SBWindow('win'),
+    SBSceneGraph('render'),
+    SBCamera('cam'),
+    SBDisplayRegion('win_dr3d', 'win', 'cam', dimensions=Vec4(0.0, 0.5, 0.0, 1.0))
+]
+SpecBase(first_spec)
 
 # Now that the renderer exists, we can set up the scene.
 base.cam.reparent_to(base.render)
@@ -23,13 +22,13 @@ model.reparent_to(base.render)
 base.step()  # Let's render what's there already.
 
 # We can change the spec at runtime by providing the intended state.
-new_spec = {
-    RendererElement.WINDOW: [SBKeep('win'), SBWindow('win_2')],
-    RendererElement.SCENE_GRAPH: [SBKeep('render')],
-    RendererElement.CAMERA: [SBKeep('cam'), SBCamera('cam_2')],
-    RendererElement.DISPLAY_REGION: [SBKeep('win_dr3d'), SBDisplayRegion('win_dr3d_2', 'win_2', 'cam_2', dimensions=Vec4(0.5, 1.0, 0.0, 1.0))],
-}
-base.respec(new_spec)
+second_spec = first_spec + [
+    SBWindow('win_2'),
+    SBCamera('cam_2'),
+    SBDisplayRegion('win_dr3d_2', 'win_2', 'cam_2', dimensions=Vec4(0.5, 1.0, 0.0, 1.0)),
+]
+
+base.respec(second_spec)
 
 
 base.cam_2.reparent_to(base.render)
@@ -40,13 +39,13 @@ base.step()
 
 
 # Let's close the first window.
-new_spec = {
-    RendererElement.WINDOW: [SBKeep('win_2')],
-    RendererElement.SCENE_GRAPH: [SBKeep('render')],
-    RendererElement.CAMERA: [SBKeep('cam_2')],
-    RendererElement.DISPLAY_REGION: [SBKeep('win_dr3d_2')],
-}
-base.respec(new_spec)
+third_spec = [
+    SBWindow('win_2'),
+    SBSceneGraph('render'),
+    SBCamera('cam_2'),
+    SBDisplayRegion('win_dr3d_2'),
+]
+base.respec(third_spec)
 
 
 base.run()
--- specbase/__init__.py
+++ specbase/__init__.py
@@ -13,156 +13,6 @@ from panda3d.core import LoaderOptions
 from panda3d.core import Filename
 
 
-class RendererElement(enum.Enum):
-    WINDOW = 1
-    SCENE_GRAPH = 2
-    CAMERA = 3
-    DISPLAY_REGION = 4
-
-
-class SpecBase:
-    def __init__(self, spec=None, pipe=None, task_mgr=True, base=True):
-        if task_mgr:
-            self.task_mgr = AsyncTaskManager.get_global_ptr()
-        self.engine = GraphicsEngine.get_global_ptr()
-
-        available_pipes = GraphicsPipeSelection.get_global_ptr()
-        available_pipes.print_pipe_types()
-        if pipe is None:
-            self.pipe = available_pipes.make_default_pipe()
-        else:
-            self.pipe = available_pipes.make_module_pipe(pipe)
-        self.loader = Loader.getGlobalPtr()
-        self._setup = {
-            RendererElement.WINDOW: {},
-            RendererElement.SCENE_GRAPH: {},
-            RendererElement.CAMERA: {},
-            RendererElement.DISPLAY_REGION: {},
-        }
-        if base:
-            __builtins__["base"] = self
-        if spec is not None:
-            self.respec(spec)
-
-    def step(self):
-        self.engine.render_frame()
-        self.engine.flip_frame()
-
-    def run(self):
-        while True:
-            self.step()
-
-    def respec(self, spec):
-        sb_element_types_forward = [
-            RendererElement.WINDOW,
-            RendererElement.SCENE_GRAPH,
-            RendererElement.CAMERA,
-            RendererElement.DISPLAY_REGION,
-        ]
-        next_state_elements = {et: [] for et in sb_element_types_forward}
-        for sb_element_type in sb_element_types_forward:
-            for sb_element in spec[sb_element_type]:
-                element_name = sb_element.name
-                next_state_elements[sb_element_type].append(element_name)
-                if isinstance(sb_element, SBKeep):
-                    continue
-                if element_name not in self._setup[sb_element_type]:
-                    new_element = self.add_element(sb_element)
-                    self._setup[sb_element_type][element_name] = new_element
-                    if sb_element.add_to_base:
-                        setattr(self, element_name, new_element)
-
-        sb_element_types_backward = [
-            RendererElement.DISPLAY_REGION,
-            RendererElement.CAMERA,
-            RendererElement.SCENE_GRAPH,
-            RendererElement.WINDOW,
-        ]
-        nodes_to_delete = []
-        for sb_element_type in sb_element_types_backward:
-            for name in self._setup[sb_element_type]:
-                if name not in next_state_elements[sb_element_type]:
-                    nodes_to_delete.append((sb_element_type, name))
-        for sb_element_type, name in nodes_to_delete:
-            self.del_element(sb_element_type, name)
-
-    def add_element(self, sb_element):
-        if isinstance(sb_element, SBWindow):
-            return self.add_window(sb_element)
-        elif isinstance(sb_element, SBSceneGraph):
-            return self.add_scene_graph(sb_element)
-        elif isinstance(sb_element, SBCamera):
-            return self.add_camera(sb_element)
-        elif isinstance(sb_element, SBDisplayRegion):
-            return self.add_display_region(sb_element)
-        else:
-            raise ValueError(f"Unknown element type {type(sb_element)} for name {sb_element.name}")
-
-    def del_element(self, sb_element_type, name):
-        if sb_element_type == RendererElement.WINDOW:
-            self.del_window(name)
-        elif sb_element_type == RendererElement.SCENE_GRAPH:
-            self.del_scene_graph(name)
-        elif sb_element_type == RendererElement.CAMERA:
-            self.del_camera(name)
-        elif sb_element_type == RendererElement.DISPLAY_REGION:
-            self.del_display_region(name)
-        else:
-            raise ValueError(f"Unknown element type {sb_element_type}")
-
-    def add_window(self, sb_window):
-        window = self.engine.make_output(
-            self.pipe,
-            sb_window.name,
-            sb_window.sort,
-            sb_window.fbprops,
-            sb_window.wprops,
-            sb_window.flags,
-        )
-        return window
-
-    def del_window(self, name):
-        win = self._setup[RendererElement.WINDOW][name]
-        base.engine.remove_window(win)
-
-    def add_scene_graph(self, sb_element):
-        sg_name = sb_element.name
-        np = NodePath(sg_name)
-        return np
-
-    def del_scene_graph(self, name):
-        sg = self._setup[RendererElement.SCENE_GRAPH][name]
-        sg.remove_node()
-        del self._setup[RendererElement.SCENE_GRAPH][name]
-
-    def add_camera(self, sb_element):
-        cam_name = sb_element.name
-        camera = Camera(cam_name)
-        camera_np = NodePath(camera)
-        return camera_np
-
-    def del_camera(self, name):
-        cam = self._setup[RendererElement.CAMERA][name]
-        cam.remove_node()
-        del self._setup[RendererElement.CAMERA][name]
-
-    def add_display_region(self, sb_element):
-        dr_name = sb_element.name
-        win_name = sb_element.window_name
-        cam_name = sb_element.camera_name
-        win = self._setup[RendererElement.WINDOW][win_name]
-        cam = self._setup[RendererElement.CAMERA][cam_name]
-        dr = win.make_display_region()
-        dr.set_camera(cam)
-        if sb_element.dimensions is not None:
-            dr.dimensions = sb_element.dimensions
-        return dr
-
-    def del_display_region(self, name):
-        dr = self._setup[RendererElement.DISPLAY_REGION][name]
-        dr.window.remove_display_region(dr)
-
-
 class SBWindow:
     def __init__(self, name, add_to_base=True):
         self.name = name
@@ -186,7 +36,7 @@ class SBCamera:
 
 
 class SBDisplayRegion:
-    def __init__(self, name, window_name, camera_name, dimensions=None, add_to_base=True):
+    def __init__(self, name, window_name=None, camera_name=None, dimensions=None, add_to_base=True):
         self.name = name
         self.window_name = window_name
         self.camera_name = camera_name
@@ -194,9 +44,145 @@ class SBDisplayRegion:
         self.add_to_base = add_to_base
 
 
-class SBKeep:
-    def __init__(self, name):
-        self.name = name
+class SpecBase:
+    def __init__(self, spec=None, pipe=None, task_mgr=True, base=True):
+        if task_mgr:
+            self.task_mgr = AsyncTaskManager.get_global_ptr()
+        self.engine = GraphicsEngine.get_global_ptr()
+
+        available_pipes = GraphicsPipeSelection.get_global_ptr()
+        available_pipes.print_pipe_types()
+        if pipe is None:
+            self.pipe = available_pipes.make_default_pipe()
+        else:
+            self.pipe = available_pipes.make_module_pipe(pipe)
+        self.loader = Loader.getGlobalPtr()
+        self._setup = {}
+        if base:
+            __builtins__["base"] = self
+        if spec is not None:
+            self.respec(spec)
+
+    def step(self):
+        self.engine.render_frame()
+        self.engine.flip_frame()
+
+    def run(self):
+        while True:
+            self.step()
+
+    def respec(self, spec):
+        # First, we add all new elements *except* DisplayRegions, and
+        # then those too.
+        for sb_element in [elem for elem in spec if not isinstance(elem, SBDisplayRegion)]:
+            self.process_for_addition(sb_element)
+        for sb_element in [elem for elem in spec if isinstance(elem, SBDisplayRegion)]:
+            self.process_for_addition(sb_element)
+
+        next_state_names = set([elem.name for elem in spec])
+        current_state_names = set(self._setup.keys())
+        obsolete_names = current_state_names - next_state_names
+        obsolete_states = [self._setup[name][0] for name in obsolete_names]
+        for sb_element in [elem for elem in obsolete_states if isinstance(elem, SBDisplayRegion)]:
+            self.process_for_deletion(sb_element)
+        for sb_element in [elem for elem in obsolete_states if not isinstance(elem, SBDisplayRegion)]:
+            self.process_for_deletion(sb_element)
+
+    def process_for_addition(self, sb_element):
+        element_name = sb_element.name
+        # If it exists already, skip it.
+        if element_name in self._setup:
+            return
+        # It doesn't, so create it.
+        new_element = self.add_element(sb_element)
+        self._setup[element_name] = (sb_element, new_element)
+        if sb_element.add_to_base:
+            setattr(self, element_name, new_element)
+
+    def process_for_deletion(self, sb_element):
+        # We need to delete the object itself, its _setup entry, and the
+        # `base` attribute.
+        sb_element, _ = self._setup[sb_element.name]
+
+        self.del_element(sb_element)
+        if sb_element.add_to_base:
+            delattr(self, sb_element.name)
+        del self._setup[sb_element.name]
+
+    def add_element(self, sb_element):
+        if isinstance(sb_element, SBWindow):
+            return self.add_window(sb_element)
+        elif isinstance(sb_element, SBSceneGraph):
+            return self.add_scene_graph(sb_element)
+        elif isinstance(sb_element, SBCamera):
+            return self.add_camera(sb_element)
+        elif isinstance(sb_element, SBDisplayRegion):
+            return self.add_display_region(sb_element)
+        else:
+            raise ValueError(f"Unknown element type {type(sb_element)} for name {sb_element.name}")
+
+    def del_element(self, sb_element):
+        name = sb_element.name
+        if isinstance(sb_element, SBWindow):
+            self.del_window(name)
+        elif isinstance(sb_element, SBSceneGraph):
+            self.del_scene_graph(name)
+        elif isinstance(sb_element, SBCamera):
+            self.del_camera(name)
+        elif isinstance(sb_element, SBDisplayRegion):
+            self.del_display_region(name)
+        else:
+            raise ValueError(f"Unknown element type {sb_element_type}")
+
+    def add_window(self, sb_window):
+        window = self.engine.make_output(
+            self.pipe,
+            sb_window.name,
+            sb_window.sort,
+            sb_window.fbprops,
+            sb_window.wprops,
+            sb_window.flags,
+        )
+        return window
+
+    def del_window(self, name):
+        _, win = self._setup[name]
+        base.engine.remove_window(win)
+
+    def add_scene_graph(self, sb_element):
+        sg_name = sb_element.name
+        np = NodePath(sg_name)
+        return np
+
+    def del_scene_graph(self, name):
+        _, sg = self._setup[name]
+        sg.remove_node()
+
+    def add_camera(self, sb_element):
+        cam_name = sb_element.name
+        camera = Camera(cam_name)
+        camera_np = NodePath(camera)
+        return camera_np
+
+    def del_camera(self, name):
+        _, cam = self._setup[name]
+        cam.remove_node()
+
+    def add_display_region(self, sb_element):
+        dr_name = sb_element.name
+        win_name = sb_element.window_name
+        cam_name = sb_element.camera_name
+        _, win = self._setup[win_name]
+        _, cam = self._setup[cam_name]
+        dr = win.make_display_region()
+        dr.set_camera(cam)
+        if sb_element.dimensions is not None:
+            dr.dimensions = sb_element.dimensions
+        return dr
+
+    def del_display_region(self, name):
+        _, dr = self._setup[name]
+        dr.window.remove_display_region(dr)
 
 
 def load_model(name):
